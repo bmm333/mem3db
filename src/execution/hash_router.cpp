bd78ccfedaf91b837cf495f961d0be8412808331
bmm333 <81256935+bmm333@users.noreply.github.com>
2025-12-01 21:47:17 +0000

    feat: functional hash_router

--- src/execution/hash_router.cpp
+++ src/execution/hash_router.cpp
@@ -39,5 +39,52 @@ HashRouter::~HashRouter()
 
 size_t HashRouter::put_batch(const uint8_t* keys, const uint8_t* values, size_t count)
 {
-    //will continue asap
+    if(count>max_batch_) throw std::runtime_error("batch too large");
+    if(count==0)return 0;
+
+    size_t keys_bytes=count*KEY_SIZE;
+    size_t vals_bytes=count*VALUES_SIZE;
+
+    //1 copy user->Pinned
+    std::memcpy(h_stage_keys_,keys,keys_bytes);
+    std::memcpy(h_stage_vals_,values,vals_bytes);
+    //2 DMA pinned -> device
+    check(cudaMemcpyAsync(d_stage_keys_,h_stage_keys_,keys_bytes,cudaMemecpyHostToDevice,stream_),"DMA keys");
+    check(cudaMemcpyAsync(d_stage_keys_,h_stage_keys_,keys_bytes,cudaMemcpyHostToDevice,stream_),"DMA vals");
+
+    //3 Kernel
+    gpu::batch_insert_dev(table_,d_stage_keys_,d_stage_vals_,nullptr,count,d_stage_status_,stream_);
+    //4 status back
+    check(cudaMemcpyAsync(h_stage_status_, d_stage_status_, count * sizeof(uint8_t), cudaMemcpyDeviceToHost, stream_), "DMA status");
+    //5 sync (will be blocking for now , safe for mvp just to get things working)
+    check(cudaStreamSynchronize(stream_),"Sync put");
+    //6 Count
+    size_t success=0;
+    for(size_t i=0;i<count;++i)
+    {
+        if(h_stage_status_[i]==gpu::INSERT_OK||h_stage_status_[i]==gpu::INSERT_OVERWRITE)
+        {++success;}
+    }return success;
+}
+void HashRouter::get_batch(const uint8_t* keys,size_t count,uint8_t* results,bool* found)
+{
+    if(count>max_batch_)throw std::runtime_error("Batch too large");
+    if(count==0)return;
+    size_t keys_bytes=count*KEY_SIZE;
+    //1 copy keys->pinned
+    std::memcpy(h_stage_keys_,keys,keys_bytes);
+    //2 DMA pinned->device
+    check(cudaMemcpyAsync(d_stage_keys_, h_stage_keys_, keys_bytes, cudaMemcpyHostToDevice, stream_), "DMA keys");
+    //3 Kernel
+    gpu::batch_lookup_dev(table_,d_stage_keys_,nullptr,d_stage_vals_,d_stage_status_,count,stream_);
+    //4 DMA results -> pinned
+    check(cudaMemcpyAsync(h_stage_vals_, d_stage_vals_, count * VALUE_SIZE, cudaMemcpyDeviceToHost, stream_), "DMA vals back");
+    check(cudaMemcpyAsync(h_stage_status_, d_stage_status_, count * sizeof(uint8_t), cudaMemcpyDeviceToHost, stream_), "DMA status back");
+    check(cudaStreamSynchronize(stream_),"Sync get");
+    //5 copy pinned->user ouput
+    std::memcpy(results,h_stage_vals_,count*VALUES_SIZE);
+    for(size_t i=0;i<count;++i)
+    {
+        found[i]=(h_stage_status_[i]==gpu::LOOKUP_FOUND);
+    }
 }
